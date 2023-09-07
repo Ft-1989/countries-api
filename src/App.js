5b228b6d47a7a169d722bc97c946f982ffbad38b
Ft-1989 <ficatejic@gmail.com>
2023-09-07 20:55:13 +0200

    a

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <main>
           <Routes>
             <Route
-              path="/"
+              path="/countries-api"
               element={
                 <>
                   <Filter
